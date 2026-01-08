5c9b86e5142517177eb75ba6ddc746b060344581
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2026-01-08 17:53:38 +0300

    update page component to enable dynamic rendering and set revalidation to zero

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,11 +1,9 @@
 import CheckoutPage from "@/components/pages/CheckoutPage";
 import { getUserSubscription } from "@/app/checkout/actions";
-
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default async function Page() {
-  // In the future, get this from your Auth session
   const userEmail = "zak@gmail.com";
-
-  // Query Stripe for this user's current status
   const subscription = await getUserSubscription(userEmail);
 
   return (
