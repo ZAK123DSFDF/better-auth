b737e0556f40c90fef2051b2060f53e347850d4a
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2026-02-07 16:46:27 +0300

    add options to show and allow removal of discounts in Paddle component

--- src/components/pages/Paddle.tsx
+++ src/components/pages/Paddle.tsx
@@ -59,6 +59,8 @@ export default function Payment({
         displayMode: "overlay",
         theme: "light",
         successUrl,
+        showAddDiscounts: true,
+        allowDiscountRemoval: true,
       },
     });
   };
